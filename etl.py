a60cf4aeedac3316afdd15337c9e86b540100806
chhokara <chhokara@gmail.com>
2025-03-01 18:31:51 -0800

    processing events

--- etl.py
+++ etl.py
@@ -1,50 +1,27 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, ArrayType, DoubleType
-from pyspark.sql.functions import col, lower, trim, regexp_replace, to_timestamp
+from pyspark.sql.functions import col, lower, trim, regexp_replace, to_timestamp, explode
 import sys
 assert sys.version_info >= (3, 5)
 
 
 def read_data(spark, input_path_events):
-    events_schema = StructType([
-        StructField("url", StringType(), True),
-        StructField("id", StringType(), True),
-        StructField("jurisdiction_url", StringType(), True),
-        StructField("headline", StringType(), True),
-        StructField("status", StringType(), True),
-        StructField("description", StringType(), True),
-        StructField("ivr_message", StringType(), True),
-        StructField("schedule", StructType([
-            StructField("intervals", ArrayType(StringType()), True)
-        ]), True),
-        StructField("event_type", StringType(), True),
-        StructField("event_subtypes", ArrayType(StringType()), True),
-        StructField("updated", StringType(), True),
-        StructField("created", StringType(), True),
-        StructField("severity", StringType(), True),
-        StructField("geography", StructType([
-            StructField("type", StringType(), True),
-            StructField("coordinates", ArrayType(
-                    ArrayType(DoubleType())), True)
-        ]), True),
-        StructField("roads", ArrayType(StructType([
-            StructField("direction", StringType(), True),
-            StructField("from", StringType(), True),
-            StructField("name", StringType(), True),
-            StructField("to", StringType(), True),
-            StructField("state", StringType(), True),
-            StructField("delay", StringType(), True)
-        ])), True),
-        StructField("areas", ArrayType(StructType([
-            StructField("url", StringType(), True),
-            StructField("name", StringType(), True),
-            StructField("id", StringType(), True)
-        ])), True)
-    ])
+    raw_events = spark.read.json(input_path_events)
 
-    events_df = spark.read.json(input_path_events, schema=events_schema)
+    print("Schema before flattening:")
+    raw_events.printSchema()
+    raw_events.show(truncate=False)
 
-    return events_df
+    events_exploded = raw_events.select(
+        explode(col("events")).alias("events_data"))
+
+    events = events_exploded.select("events_data.*")
+
+    print("Schema after flattening:")
+    events.printSchema()
+    events.show(truncate=False)
+
+    return events
 
 
 def clean_data(events_df):
@@ -64,16 +41,71 @@ def clean_data(events_df):
     events_df = events_df.filter((col("geography.coordinates")[0][0] >= -180) & (col("geography.coordinates")[0][0] <= 180) &
                                  (col("geography.coordinates")[0][1] >= -90) & (col("geography.coordinates")[0][1] <= 90))
 
-    events_df = events_df.dropna(
-        subset=["id", "event_type", "geography.coordinates"])
+    events_df = events_df.dropna(subset=["id", "event_type"])
+
+    events_df = events_df.filter(col("geography").isNotNull())
 
     return events_df
 
 
+def define_schema():
+    events_schema = StructType([
+        StructField("url", StringType(), True),
+        StructField("id", StringType(), True),
+        StructField("jurisdiction_url", StringType(), True),
+        StructField("headline", StringType(), True),
+        StructField("status", StringType(), True),
+        StructField("description", StringType(), True),
+        StructField("ivr_message", StringType(), True),
+        StructField("linear_reference_km", DoubleType(),
+                    True),
+        StructField("schedule", StructType([
+            StructField("intervals", ArrayType(StringType()), True),
+            StructField("recurring_schedules", ArrayType(StructType([
+                StructField("daily_end_time", StringType(), True),
+                StructField("daily_start_time", StringType(), True),
+                StructField("days", ArrayType(DoubleType()), True),
+                StructField("end_date", StringType(), True),
+                StructField("start_date", StringType(), True)
+            ])), True)
+        ]), True),
+        StructField("event_type", StringType(), True),
+        StructField("event_subtypes", ArrayType(StringType()), True),
+        StructField("updated", StringType(), True),
+        StructField("created", StringType(), True),
+        StructField("severity", StringType(), True),
+        StructField("geography", StructType([
+            StructField("type", StringType(), True),
+            StructField("coordinates", ArrayType(
+                ArrayType(DoubleType())), True)
+        ]), True),
+        StructField("roads", ArrayType(StructType([
+            StructField("direction", StringType(), True),
+            StructField("from", StringType(), True),
+            StructField("name", StringType(), True),
+            StructField("to", StringType(), True),
+            StructField("state", StringType(), True),
+            StructField("delay", StringType(), True)
+        ])), True),
+        StructField("areas", ArrayType(StructType([
+            StructField("url", StringType(), True),
+            StructField("name", StringType(), True),
+            StructField("id", StringType(), True)
+        ])), True)
+    ])
+
+    return events_schema
+
+
 def main(input_path_events, output):
     events_df = read_data(spark, input_path_events)
+    events_schema = define_schema()
+    events_df = spark.createDataFrame(events_df.rdd, schema=events_schema)
     events_df = clean_data(events_df)
 
+    print("Final DF being written to parquet:")
+    events_df.printSchema()
+    events_df.show(truncate=False)
     events_df.write.mode("overwrite").parquet(output)
 
 
