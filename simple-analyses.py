8359e4dc46cd1d6b3aeee632f5b81f6e33a1875b
Alex Sichitiu <asichitiu@gmail.com>
2025-03-10 18:22:27 -0700

    Created generic by_group fn

--- simple-analyses.py
+++ simple-analyses.py
@@ -6,13 +6,20 @@ from pyspark.sql import SparkSession, functions as f
 import sys
 assert sys.version_info >= (3, 5)
 
-def by_severity(df, output):
-    grp_df = df.groupBy('severity').count()
-    grp_df.write.mode('overwrite').parquet(f'{output}/by_severity')
+def by_group(df, group, output):
+    """
+    For a given dataframe and column name, writes a parquet file with count grouping.
+    """
+    grp_df = df.groupBy(group).count()
+    grp_df.write.mode('overwrite').parquet(f'{output}/by_{group}')
+
 
 def main(inputs,  output):
     df = spark.read.parquet(inputs)
-    by_severity(df, output)
+    by_group(df, 'severity', output)
+    by_group(df, 'event_type', output)
+    by_group(df, 'road', output)
+    by_group(df, 'area', output)
 
 
 if __name__ == '__main__':
