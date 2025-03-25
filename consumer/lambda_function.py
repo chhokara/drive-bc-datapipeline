a2b54f6ee56a670fd7643e2af0eea0a3ce19013f
chhokara <chhokara@gmail.com>
2025-03-24 19:10:39 -0700

    update lambda

--- consumer/lambda_function.py
+++ consumer/lambda_function.py
@@ -3,11 +3,11 @@ import base64
 
 
 def lambda_handler(event, context):
+    print("Received data from Kinesis:")
     for record in event['Records']:
         payload = base64.b64decode(record['kinesis']['data']).decode('utf-8')
         data = json.loads(payload)
 
-        print("Received data from Kinesis:")
         print(json.dumps(data, indent=2))
 
     return {
